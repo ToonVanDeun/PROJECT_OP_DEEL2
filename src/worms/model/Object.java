74606b4ad5864b2b6afb6df0e1b5d3907a0480c8
ToonVanDeun <toon.vandeun@student.kuleuven.be>
2014-04-09 11:57:34 +0200

    nieuwe klasse Object
    ook methodes toegevoeg aan WORLD om te interageren met OBJECT

---
+++ src/worms/model/Object.java
@@ -0,0 +1,157 @@
+package worms.model;
+
+import be.kuleuven.cs.som.annotate.Basic;
+import be.kuleuven.cs.som.annotate.Model;
+import be.kuleuven.cs.som.annotate.Raw;
+
+
+/**
+ * 
+ * A class of objects that can be in a world of class World. 
+ * 
+ * @invar  Each object can only be located in at most one world.
+ *       | 
+ * @invar  No world contains te same object twice.
+ *       | 
+ *       
+ * @author Toon
+ */
+public abstract class Object {
+
+	/**
+	 * Initialize this new object with given world.
+	 *
+	 * @param  world
+	 *         The world the object is being part of.
+	 * @effect The world of this new object is set to the given
+	 *         world.
+	 *       | setOwnerTo(owner) !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	 */
+	@Raw
+	@Model
+	protected Object(World world) throws IllegalArgumentException {
+		setWorldTo(world);
+	}
+	
+	/**
+	 * Return the world of this object.
+	 *   A null reference is returned if this object is not in a world.
+	 */
+	@Basic
+	@Raw
+	public World getWorld() {
+		return this.world;
+	}
+
+	/**
+	 * Check whether this object can have the given world
+	 * as its world.
+	 *
+	 * @param  world
+	 *         The world to check.
+	 * @return True, if the object is not jet in a world.
+	 * 			| ...
+	 */
+	@Raw
+	public boolean canHaveAsWorld(World world) {
+		// nog niet juis controleert enkel of object niet in gegeven wereld zit.
+		// moet ook controleren of het niet in een andere wereld zit.
+		if (world.getAllObjects().contains(this))
+			return false;
+		return true;
+		
+	}
+
+	
+
+	/**
+	 * Check whether this object is in a world.
+	 *
+	 * @return True if and only if the world of this object is effective.
+	 *       | result == (getWorld() != null)
+	 */
+	@Raw
+	public boolean hasWorld() {
+		return getWorld() != null;
+	}
+
+	/**
+	 * Set the world of this object to the given world.
+	 *
+	 * @param  world
+	 *         The new world for this object.
+	 * @post   The world of this object is the same as the given world.
+	 *       | new.getWorld() == world
+	 * @post   The number of objects of the given world
+	 *         is incremented by 1.
+	 *       | (new world).getNbOwnings() == world.getNbOwnings() + 1
+	 * @post   The given world has this objects as its new last
+	 *         object.
+	 *       | (new world).getObjectAt(getNbObjects()+1) == this
+	 * @throws IllegalArgumentException
+	 *         The given world is not effective or it cannot have this object
+	 *         as its new last object.
+	 *       | (world == null) ||
+	 *       |   (! world.canHaveAsObjectAt(this,world.getNbObjects()+1))
+	 * @throws IllegalStateException
+	 *         This object already belongs to a world.
+	 *       | hasWorld()
+	 */
+	public void setWorldTo(World world)
+			throws IllegalArgumentException, IllegalStateException {
+		if ((world == null)
+				|| (!world.canHaveAsObjectAt(this, world.getNbObjects()+1)))
+			throw new IllegalArgumentException();
+		if (this.hasWorld())
+			throw new IllegalStateException("Already belongs to a world!");
+		setWorld(world);
+		world.addAsObject(this);
+	}
+
+	/**
+	 * Unset the world, if any, from this object.
+	 *
+	 * @post   This object no longer belongs to a world.
+	 *       | ! new.hasWorld()
+	 * @post   The former world of this object, if any, no longer
+	 *         has this object as one of its objects.
+	 *       |    (getWorld() == null)
+	 *       | || (! (new getWorld()).hasAsObject(object))
+	 * @post   All objects registered beyond the position at which
+	 *         this owning was registered shift one position to the left.
+	 *       | (getWorld() == null) ||
+	 *       | (for each index in
+	 *       |        getWorld().getIndexOfObject(object)+1..getWorld().getNbObjects():
+	 *       |    (new getWorld()).getObjectAt(index-1) == getWorld().getObjectAt(index) ) 
+	 */
+	public void unsetWorld() {
+		if (hasWorld()) {
+			World formerWorld = this.getWorld();
+			setWorld(null);
+			formerWorld.removeAsObject(this);
+		}
+	}
+
+	/**
+	 * Set the owner of this ownable to the given owner.
+	 *
+	 * @param  owner
+	 *         The new owner for this ownable.
+	 * @pre    This ownable can have the given owner as its owner.
+	 *       | canHaveAsOwner(owner)
+	 * @post   The owner of this ownable is the same as the given owner.
+	 *       | new.getOwner() == owner
+	 */
+	@Raw
+	private void setWorld(@Raw World world) {
+		assert canHaveAsWorld(world);
+		this.world = world;
+	}
+
+	/**
+	 * Variable referencing the world of this object.
+	 */
+	private World world;
+		
+	
+}
--- src/worms/model/World.java
+++ src/worms/model/World.java
@@ -5,7 +5,11 @@ package worms.model;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+
+import be.kuleuven.cs.som.annotate.Basic;
+import be.kuleuven.cs.som.annotate.Raw;
 
 
 /**
@@ -116,67 +120,246 @@ public class World {
 	public Random getPerimeter() {
 		return this.perimeter;		
 	}
-	public void addWorm(Worm worm){
-		worms.add(worm);
-	}
-	public void removeWorm(Worm worm) {
-		worms.remove(worm);
-	}
-	public ArrayList<Worm> getWorms(){
-		return worms;
-	}
 	
 	
-	private int getCurrentWormIndex() {
-		return currentWormIndex;
-	}
+//	private int getCurrentWormIndex() {
+//		return currentWormIndex;
+//	}
+//
+//	private void setCurrentWormIndex(int currentWormIndex) {
+//		this.currentWormIndex = currentWormIndex;
+//	}
+//
+//	public Worm getCurrentWorm(){
+//		return this.worms.get(this.getCurrentWormIndex());
+//	}
+//
+//	public void startNextTurn(){
+//		if (getCurrentWormIndex() >= (worms.size()-1))
+//			startNextRound();
+//		else
+//			setCurrentWormIndex(getCurrentWormIndex()+1);
+//
+//	}
+//
+//	private void startNextRound(){
+//
+//		for (Worm worm: worms){
+//			worm.newRound();
+//		}
+//		setCurrentWormIndex(0);
+//
+//	}
+//
+//	public void	startGame(){
+//		setCurrentWormIndex(0);
+//	}
 
-	private void setCurrentWormIndex(int currentWormIndex) {
-		this.currentWormIndex = currentWormIndex;
-	}
-
-	public Worm getCurrentWorm(){
-		return this.worms.get(this.getCurrentWormIndex());
-	}
-
-	public void startNextTurn(){
-		if (getCurrentWormIndex() >= (worms.size()-1))
-			startNextRound();
-		else
-			setCurrentWormIndex(getCurrentWormIndex()+1);
-
-	}
-
-	private void startNextRound(){
-
-		for (Worm worm: worms){
-			worm.newRound();
-		}
-		setCurrentWormIndex(0);
-
-	}
-
-	public void	startGame(){
-		setCurrentWormIndex(0);
-	}
-
-	public void addFood(Food food) {
-			foods.add(food);
-		}
-		public ArrayList<Food> getFood() {
-			return foods;
-		}
-		public void removeFood(Food food) {
-			foods.remove(food);
-		}
+	
 		
-	private ArrayList<Food> foods = new ArrayList<Food>();
-	private ArrayList<Worm> worms = new ArrayList<Worm>();
-	private int currentWormIndex;
+//	private ArrayList<Food> foods = new ArrayList<Food>();
+//	private ArrayList<Worm> worms = new ArrayList<Worm>();
+//	private int currentWormIndex;
+	
+	/**
+	 * Return the object of this world at the given index.
+	 * 
+	 * @param  index
+	 *         The index of the owning to return.
+	 * @throws IndexOutOfBoundsException
+	 *         The given index is not positive or it exceeds the
+	 *         number of objects of this person.
+	 *       | (index < 1) || (index > getNbObjects())
+	 */
+	@Basic
+	@Raw
+	public Object getObjectAt(int index) throws IndexOutOfBoundsException {
+		return objects.get(index - 1);
+	}
+
+	/**
+	 * Return the number of objects in this world.
+	 */
+	@Basic
+	@Raw
+	public int getNbObjects() {
+		return objects.size();
+	}
+	
+	/**
+	 * Check whether this world can have the given object
+	 * as one of its objects.
+	 * 
+	 * @param  object
+	 *         The object to check.
+	 * @return True if and only if the given object is effective, and
+	 *         if that object can have this world as its world.
+	 *       | result ==
+	 *       |   (object != null) &&
+	 *       |   object.canHaveAsWorld(this)
+	 */
+	@Raw
+	public boolean canHaveAsObject(Object object) {
+		return (object != null) && object.canHaveAsWorld(this);
+	}
+
+	/**
+	 * Check whether this world can have the given object
+	 * as one of its objects at the given index.
+	 * 
+	 * @param  object
+	 *         The object to check.
+	 * @param  index
+	 *         The index to check.
+	 * @return False if the given index is not positive or exceeds
+	 *         the number of objects of this world + 1.
+	 *       | if ( (index < 1) || (index > getNbObjects()+1) )
+	 *       |   then result == false
+	 *         Otherwise, false if this world cannot have the
+	 *         given object as one of its objects.
+	 *       | else if (! canHaveAsObject(object))
+	 *       |   then result == false
+	 *         Otherwise, true if and only if the given object is
+	 *         not already registered at another index.
+	 *       | else result ==
+	 *       |   for each I in 1..getNbObjects():
+	 *       |     ( (I == index) || (getObjectAt(I) != object) )
+	 */
+	@Raw
+	public boolean canHaveAsObjectAt(Object object, int index) {
+		if ((index < 1) || (index > getNbObjects() + 1))
+			return false;
+		if (!canHaveAsObject(object))
+			return false;
+		for (int pos = 1; pos <= getNbObjects(); pos++)
+			if ((pos != index) && (getObjectAt(pos) == object))
+				return false;
+		return true;
+	}
+	
+	/**
+	 * Check whether this world has the given object as one of
+	 * its objects.
+	 *
+	 * @param  object
+	 *         The object to check.
+	 * @return True if and only if this world has the given object
+	 *         as one of its objects at some index.
+	 *       | result ==
+	 *       |   for some index in 1..getNbObjects():
+	 *       |     getObjectAt(index).equals(object)
+	 */
+	@Raw
+	public boolean hasAsObject(Object object) {
+		return objects.contains(object);
+	}
+
+	/**
+	 * Return the index at which the given object is registered
+	 * in the list of objects for this world.
+	 *  
+	 * @param  object
+	 *         The object to search for.
+	 * @return If this world has the given object as one of its
+	 *         objects, that object is registered at the resulting
+	 *         index. Otherwise, the resulting value is -1.
+	 *       | if (hasAsObject(object))
+	 *       |    then getObjectAt(result) == object
+	 *       |    else result == -1
+	 */
+	@Raw
+	public int getIndexOfObject(Object object) {
+		return objects.indexOf(object);
+	}
+	
+	/**
+	 * Return a list of all the objects of this world.
+	 * 
+	 * @return The size of the resulting list is equal to the number of
+	 *         objects in this world.
+	 *       | result.size() == getNbObjects()
+	 * @return Each element in the resulting list is the same as the
+	 *         objects of this world at the corresponding index.
+	 *       | for each index in 0..result-size()-1 :
+	 *       |   result.get(index) == getObjectAt(index+1)
+	 */
+	public List<Object> getAllObjects() {
+		return new ArrayList<Object>(objects);
+	}
+	
+	/**
+	 * Add the given object at the end of the list of
+	 * objects of this world.
+	 * 
+	 * @param  object
+	 *         The object to be added.
+	 * @pre    The given object is effective and already references
+	 *         this world as its world.
+	 *       | (world != null) && (object.getWorld() == this)
+	 * @pre    This world does not yet have the given object
+	 *         as one of its objects.
+	 *       | ! hasAsObject(object)
+	 * @post   The number of objects of this world is incremented
+	 *         by 1.
+	 *       | new.getNbObjects() == getNbObjects() + 1
+	 * @post   This world has the given object as its new last
+	 *         object.
+	 *       | new.getObjectAt(getNbObjects()+1) == object
+	 */
+	public void addAsObject(@Raw Object object) {
+		assert (object != null) && (object.getWorld() == this);
+		assert !hasAsObject(object);
+		objects.add(object);
+	}
+
+	/**
+	 * Remove the given object from the objects of this world.
+	 * 
+	 * @param  object
+	 *         The object to be removed.
+	 * @pre    The given object is effective and does not have any
+	 *         world.
+	 *       | (object != null) && (object.getWorld() == null)
+	 * @pre    This world has the given object as one of
+	 *         its objects.
+	 *       | hasAsObject(object)
+	 * @post   The number of objects of this world is decremented
+	 *         by 1.
+	 *       | new.getNbObjects() == getNbObjects() - 1
+	 * @post   This world no longer has the given object as
+	 *         one of its objects.
+	 *       | (! new.hasAsObject(object))
+	 * @post   All objects registered beyond the removed object
+	 *         shift one position to the left.
+	 *       | for each index in getIndexOfObject(object)+1..getNbObjects():
+	 *       |   new.getObjectAt(index-1) == getObjectAt(index) 
+	 */
+	@Raw
+	public void removeAsObject(Object object) {
+		assert (object != null) && (object.getWorld() == null);
+		assert (hasAsObject(object));
+		objects.remove(object);
+	}
+	
+	
+	
+	
+	
+	/**
+	 * Variable referencing a list collecting all the objects of
+	 * this world.
+	 * @invar  Each object registered in the referenced list is
+	 *         effective and not yet terminated.
+	 *       | for each object in objects:
+	 *       |   ( (object != null) && (!object.isTerminated()) )
+	 */
+	private  List<Object> objects = new ArrayList<Object>();
+	
 	private final Random perimeter;
 	private boolean[][] passableMap;
 	private double width;
 	private double height;
 	private double upperboundWidth;
 	private double upperboundHeight;
+	
 }
